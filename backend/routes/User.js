42092b622e752220726975ab5d26220c7467ca97
Rutvija Mali <rutvijamali@gmail.com>
2025-04-29 23:34:47 +0530

    Changed bcrypt liabrary

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import User from '../models/User.js';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import errorLogger from '../middlewares/errorLogger.js'
